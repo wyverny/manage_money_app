33b513ae38970acdb7e5df0c2fd470c47d360d3e
wyverny <wyvernyy@hotmail.com>
2012-10-31 00:27:33 +0900

    added required gui files

---
+++ src/com/lcm/data/MonthlyStat.java
@@ -0,0 +1,34 @@
+package com.lcm.data;
+
+import java.text.DecimalFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.lcm.smsSmini.R;
+import com.lcm.view.GsCalendar;
+import com.lcm.view.MainActivity;
+import com.lcm.view.Util;
+
+public class MonthlyStat {
+	private static final String TAG = "MonthlyStat";
+	DecimalFormat decimalFormat = new DecimalFormat("#,#00 ¿ø");
+	MonthlyData monthlyData;
+	Context context;
+	
+	public MonthlyStat(Context context, MonthlyData monthlyData) {
+		this.monthlyData = monthlyData;
+		this.context = context;
+	}
+}
